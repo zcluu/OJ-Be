286b6bdc04190bc37b262c1e0b630be560e232c1
DMSintalor <1256234567@qq.com>
2023-07-04 19:06:19 +0800

    judge and isolate db session in threads

--- OJ/views/submission.py
+++ OJ/views/submission.py
@@ -1,8 +1,10 @@
+from threading import Thread
+
 from fastapi import APIRouter, Depends, Header, status, Response
 
 from sqlalchemy.orm import Session
 
-from OJ.db.database import get_session
+from OJ.db.database import get_session, SessionLocal
 
 from OJ.util.controller import get_user
 from OJ.util.judge import JudgeDispatcher
@@ -20,12 +22,12 @@ router = APIRouter(
 
 
 @router.post("/judge")
-async def judge(form: JudgeForm, token: Union[str, None] = Header(None), db: Session = Depends(get_session)):
+async def judge(form: JudgeForm, x_token: Union[str, None] = Header(None), db: Session = Depends(get_session)):
     problem = db.query(ProblemInfo).filter_by(id=form.problem_id).first()
     if not problem:
         return Response(status_code=status.HTTP_404_NOT_FOUND)
     language = form.language
-    user = get_user(token)
+    user = get_user(x_token)
     user_id = user.id
     code_source = form.source_code
     submission = Submission(language=language,
@@ -35,12 +37,20 @@ async def judge(form: JudgeForm, token: Union[str, None] = Header(None), db: Ses
                             contest_id=problem.contest_id)
     db.add(submission)
     db.commit()
-    JudgeDispatcher(submission.id, problem.id).judge()
+    thread = Thread(target=SubmissionJudge, args=(submission.id, problem.id))
+    thread.start()
+    # JudgeDispatcher(submission.id, problem.id, db).judge()
+    return submission.id
+
+
+def SubmissionJudge(sid, pid):
+    sess = SessionLocal()
+    dis = JudgeDispatcher(sid, pid, sess)
+    dis.judge()
 
 
 @router.get("/status")
 async def submission_status(submission_id: int,
-                            token: Union[str, None] = Header(None),
                             db: Session = Depends(get_session)):
     submission = db.query(Submission).filter_by(id=submission_id).first()
     if not submission:
