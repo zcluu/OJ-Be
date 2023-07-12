db84ae8aecd7ccbdbba5698bc28beff73a8c3ac5
DMSintalor <1256234567@qq.com>
2023-07-12 22:56:00 +0800

    return blank object if token is invalid

--- OJ/util/controller.py
+++ OJ/util/controller.py
@@ -1,14 +1,16 @@
 from typing import Optional, Any, Union
-from OJ.models.UserModels import UserSession
+from OJ.models.UserModels import UserSession, UserInfo
 
 from sqlalchemy.orm import Session
 from OJ.db.database import engine
 
 
-def get_user(token) -> Optional[Any]:
+def get_user(token) -> UserInfo:
     with Session(engine) as session:
         session.begin()
         sess = session.query(UserSession).filter(UserSession.token == token).first()
+        if not sess:
+            return UserInfo()
         user = sess.sess2user
         session.close()
         return user
